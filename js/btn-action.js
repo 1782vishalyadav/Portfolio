549978c68c044c40647add70247ecee095c898a3
1782vishalyadav <1782vishalyadav@gmail.com>
2025-01-03 02:52:22 +0530

    button fix

--- js/btn-action.js
+++ js/btn-action.js
@@ -1,4 +1,4 @@
-document.getElementById('hire-me-btn1').addEventListener('click', function() {
+document.getElementById('hire-me-btn').addEventListener('click', function() {
     // Custom message or action when the button is clicked
     alert("Thank you for considering me! I will get back to you as soon as possible.");
     // Redirect to an email client with a pre-filled subject
@@ -11,3 +11,9 @@ document.getElementById('hire-me-btn2').addEventListener('click', function() {
     // Redirect to an email client with a pre-filled subject
     window.location.href = 'mailto:work.vishalyadav@gmail.com?subject=Inquiry from Portfolio';
 });
+
+
+
+
+
+

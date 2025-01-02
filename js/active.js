e1b616192723a4295046c68cf51c1e4c768789e4
1782vishalyadav <1782vishalyadav@gmail.com>
2025-01-03 02:29:52 +0530

    Buttons fix

--- js/active.js
+++ js/active.js
@@ -16,17 +16,7 @@
       }
     });
 
-    document.getElementsByClassName('hire-me-btn').addEventListener('click', function() {
-      // Custom message or action when the button is clicked
-      alert("Thank you for considering me! I will get back to you as soon as possible.");
-    });
 
-
-    // script.js
-document.getElementsByClassName('hire-me-btn').addEventListener('click', function() {
-  // Redirect to an email client with a pre-filled subject
-  window.location.href = 'mailto:work.vishalyadav@gmail.com?subject=Inquiry from Portfolio';
-});
 
 
 
---
+++ js/btn-action.js
@@ -0,0 +1,13 @@
+document.getElementById('hire-me-btn1').addEventListener('click', function() {
+    // Custom message or action when the button is clicked
+    alert("Thank you for considering me! I will get back to you as soon as possible.");
+    // Redirect to an email client with a pre-filled subject
+    window.location.href = 'mailto:work.vishalyadav@gmail.com?subject=Inquiry from Portfolio';
+});
+
+document.getElementById('hire-me-btn2').addEventListener('click', function() {
+    // Custom message or action when the button is clicked
+    alert("Thank you for considering me! I will get back to you as soon as possible.");
+    // Redirect to an email client with a pre-filled subject
+    window.location.href = 'mailto:work.vishalyadav@gmail.com?subject=Inquiry from Portfolio';
+});
